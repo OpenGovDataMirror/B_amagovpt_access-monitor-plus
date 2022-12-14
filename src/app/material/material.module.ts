a322c4a661f66468069947e79c5df8afdb3d3a59
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-14 09:44:53 +0200

    init

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { A11yModule } from '@angular/cdk/a11y';
+
+@NgModule({
+  imports: [
+    A11yModule
+  ],
+  exports: [
+    A11yModule
+  ],
+  declarations: []
+})
+export class MaterialModule {}
